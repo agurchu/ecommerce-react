d4b6bcbc855eb4adbf5f56b86065f99315c07673
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-03 17:10:56 +0200

    creating products file

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import styled from "styled-components";
 
 function Shop() {
-  return <div>Shop</div>;
+  return (
+    <ShopContainer>
+      <ShopTitle>
+        <h1>Katlego J Dev Shop</h1>
+      </ShopTitle>
+
+      <Products></Products>
+    </ShopContainer>
+  );
 }
 
 export default Shop;
+const ShopContainer = styled.div``;
+const ShopTitle = styled.div`
+  h1 {
+    font-family: var(--font-family);
+  }
+`;
+const Products = styled.div``;
---
+++ src/products.js
@@ -0,0 +1,75 @@
+const url = {
+  product1: "",
+  product2: "",
+  product3: "",
+  product4: "",
+  product5: "",
+  product6: "",
+  product7: "",
+  product8: "",
+  product9: "",
+  product10: "",
+};
+
+export const products = [
+  {
+    id: 1,
+    productName: "Samsung",
+    price: 6_999.0,
+    productImage: url.product1,
+  },
+  {
+    id: 2,
+    productName: "Samsung",
+    price: 6_999.0,
+    productImage: url.product2,
+  },
+  {
+    id: 3,
+    productName: "Samsung",
+    price: 6_999.0,
+    productImage: url.product3,
+  },
+  {
+    id: 4,
+    productName: "Samsung",
+    price: 6_999.0,
+    productImage: url.product4,
+  },
+  {
+    id: 5,
+    productName: "Samsung",
+    price: 6_999.0,
+    productImage: url.product5,
+  },
+  {
+    id: 6,
+    productName: "Samsung",
+    price: 6_999.0,
+    productImage: url.product6,
+  },
+  {
+    id: 7,
+    productName: "Samsung",
+    price: 6_999.0,
+    productImage: url.product7,
+  },
+  {
+    id: 8,
+    productName: "Samsung",
+    price: 6_999.0,
+    productImage: url.product8,
+  },
+  {
+    id: 9,
+    productName: "Samsung",
+    price: 6_999.0,
+    productImage: url.product9,
+  },
+  {
+    id: 10,
+    productName: "Samsung",
+    price: 6_999.0,
+    productImage: url.product10,
+  },
+];
