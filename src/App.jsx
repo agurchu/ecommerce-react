b83539ebfcdf0dd4ccfaf70d7513ab965a4840da
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-03 16:36:24 +0200

    navbar jsx and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
-          <Route />
-          <Route />
+          <Route path="/" />
+          <Route path="/cart" />
         </Routes>
       </Router>
     </div>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { ShoppingCart } from "phosphor-react";
+
+function Navbar() {
+  return (
+    <HeaderNavbar className="container">
+      <Logo to="/">
+        <img src="kj_dev_long_logo_white.png" alt="" />
+      </Logo>
+      <NavLinks>
+        <Link to="/">Shop</Link>
+        <Link to="/cart">
+          <ShoppingCart size={30} />
+        </Link>
+      </NavLinks>
+    </HeaderNavbar>
+  );
+}
+
+export default Navbar;
+
+const HeaderNavbar = styled.nav`
+  background: var(--color-text-01);
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const NavLinks = styled.ul`
+  align-items: center;
+  display: flex;
+
+  a {
+    color: var(--color-primary);
+    font-size: 2.5rem;
+    margin-left: 25px;
+    transition: all 0.25s;
+
+    &:hover {
+      color: var(--color-secondary);
+    }
+  }
+`;
+
+const Logo = styled(Link)`
+  height: 45px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    transition: all 0.25s;
+
+    &:hover {
+      filter: brightness(80%);
+    }
+  }
+`;
