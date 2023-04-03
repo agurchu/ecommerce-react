ed0b01b347dd3e5ae0ee384e6b10f3ad77fa2692
Katlego J. Mtimane <115998325+agurchu@users.noreply.github.com>
2023-04-03 15:24:06 +0200

    Initialize

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route />
+          <Route />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
